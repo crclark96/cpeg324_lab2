47592c0437a55cd3d0ac478919a6e8c8d9264de4
Collin Clark <crclark96@gmail.com>
2018-03-14 15:15:39 -0400

    tentative shift_reg design

--- shift_reg.vhdl
+++ shift_reg.vhdl
@@ -23,15 +23,58 @@ architecture behav of shift_reg is
          output : out std_logic_vector (N downto 0)
          );
   end component;
+
+  component dff
+    port(clk : in std_logic;
+         rst : in std_logic;
+         pre : in std_logic;
+         ce : in std_logic;
+         d : in std_logic;
+         q : out std_logic
+         );
+  end component;
+  
   
   signal hold, l_shf, r_shf, tmp, res : std_logic_vector (3 downto 0);
+  
 begin
+
+  l_shf(0) <= I_SHIFT_IN;
+  l_shf(1) <= hold(0);
+  l_shf(2) <= hold(1);
+  l_shf(3) <= hold(2);
+
+  r_shf(0) <= hold(1);
+  r_shf(1) <= hold(2);
+  r_shf(2) <= hold(3);
+  r_shf(3) <= I_SHIFT_IN;
+
+  process (tmp, enable) is
+  begin
+    for i in 0 to 3 loop
+      res(i) <= enable and tmp(i);
+    end loop;
+  end process;
+
+  DFFS:
+  for i in 0 to 3 generate
+    DFFX : dff port map
+      (clock, '0', '0', enable, res(i), hold(i));
+  end generate DFFS;
+       
   mux0 : mux_4_1 port map(switch => sel,
                           in0 => I,
                           in1 => l_shf,
                           in2 => r_shf,
                           in3 => hold,
                           output => tmp);
-  O <= tmp;
+
+  process (hold, enable)
+  begin
+    for i in 0 to 3 loop
+      O(i) <= enable and hold(i);
+    end loop;
+  end process;
+  
 end behav;
 
